36d447b470f2983aa6fc44ae3f8a5e09ffec816c
OrigaminatorX Pop OS! vs code <origaminatorx94@gmail.com>
2026-05-14 12:34:22 +0200

    correction des id

--- src/strategy.cpp
+++ src/strategy.cpp
@@ -124,9 +124,9 @@ void TaskStrategy(void* pvParameters)
 
     // start_straight_line(SIDE_BLUE);
     int inverse = current_side == SIDE_YELLOW ? 1 : -1;
-    if (id == 1)
+    if (id == 0)
         start_strat_pami_evo_1(inverse);
-    else if (id == 2)
+    else if (id == 1)
         start_strat_pami_evo_2(inverse);
     else if (id == 4) // debug
         start_straight_line(inverse);
